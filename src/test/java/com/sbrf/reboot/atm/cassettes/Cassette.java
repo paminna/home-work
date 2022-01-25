18cd36a895b20bb91013e8566a4f8d3c7068407b
paminna <ribekks@gmail.com>
2022-01-25 22:25:02 +0300

    fixed

---
+++ src/test/java/com/sbrf/reboot/atm/cassettes/Cassette.java
@@ -0,0 +1,16 @@
+package com.sbrf.reboot.atm.cassettes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cassette <T> extends Banknote{
+    private List<T> banknote;
+
+    public Cassette(List<T> banknote) {
+        this.banknote = banknote;
+    }
+
+    int getCountBanknotes() {
+        return banknote.size();
+    }
+}
