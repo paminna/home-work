57eb7f08e002192876b6020409c36bd6d419167b
paminna <ribekks@gmail.com>
2022-01-28 18:36:37 +0300

    done homework 7

--- src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
+++ src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class EqualsHashCodeTest {
 
@@ -14,17 +15,48 @@ public class EqualsHashCodeTest {
         Calendar releaseDate;
         int maxSpeed;
 
+        String getModel()
+        {
+            return model;
+        }
+
+        String getColor()
+        {
+            return color;
+        }
+
+        Calendar getDate()
+        {
+            return releaseDate;
+        }
+
+        int getMaxSpeed()
+        {
+            return maxSpeed;
+        }
+
         @Override
         public boolean equals(Object o) {
-
             //Рефлексивность: объект должен равняться самому себе
             if (o == this)
                 return true;
-
+            Car car = (Car)o;
+            if (       (car.getModel().equals(this.model))
+                    && (car.getMaxSpeed() == this.maxSpeed)
+                    && (car.getDate().equals(this.releaseDate))
+                    && (car.getColor().equals(car.color))
+                    && (car.getClass().equals(this.getClass())))
+                return true;
+            if (o == null || o.getClass() != this.getClass())
+                return false;
             return false;
         }
 
-
+        @Override
+        public int hashCode()
+        {
+            return Objects.hash(model, color, releaseDate, maxSpeed);
+        }
      }
 
     @Test
@@ -41,7 +73,7 @@ public class EqualsHashCodeTest {
         car2.releaseDate = new GregorianCalendar(2020, 0, 25);
         car2.maxSpeed = 10;
 
-
+        car1.equals(car2);
         Assertions.assertTrue(car1.equals(car2));
     }
 
@@ -98,5 +130,37 @@ public class EqualsHashCodeTest {
 
     }
 
+    @Test
+    public void CheckEquals(){
+        Car car1 = new Car();
+        car1.model = "audii";
+        car1.color = "white";
+        car1.releaseDate = new GregorianCalendar(2017, 0, 25);
+        car1.maxSpeed = 10;
 
+        Car car2 = new Car();
+        car2.model = "Audi";
+        car2.color = "white";
+        car2.releaseDate = new GregorianCalendar(2017, 0, 25);
+        car2.maxSpeed = 10;
+
+        Assertions.assertNotEquals(car1.hashCode(),car2.hashCode());
+    }
+
+    @Test
+    public void CheckEqualsNull(){
+        Car car1 = new Car();
+        car1.model = null;
+        car1.color = null;
+        car1.releaseDate = new GregorianCalendar(0000, 0, 00);
+        car1.maxSpeed = 0;
+
+        Car car2 = new Car();
+        car2.model = "Audi";
+        car2.color = "white";
+        car2.releaseDate = new GregorianCalendar(2017, 0, 25);
+        car2.maxSpeed = 10;
+
+        Assertions.assertNotEquals(car1.hashCode(),car2.hashCode());
+    }
 }
